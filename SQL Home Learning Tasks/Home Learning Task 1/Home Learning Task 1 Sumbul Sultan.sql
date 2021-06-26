9b5f2192af7216b434f5c15eac85ea87fffdbbca
SumbulSultan <sumbulsultan64@googlemail.com>
2021-06-26 22:30:26 +0100

    Update Home Learning Task 1 Sumbul Sultan.sql

--- SQL Home Learning Tasks/Home Learning Task 1/Home Learning Task 1 Sumbul Sultan.sql
+++ SQL Home Learning Tasks/Home Learning Task 1/Home Learning Task 1 Sumbul Sultan.sql
@@ -1,2 +1,12 @@
-
-
+create database cars;
+use cars;
+create table cars_details(
+car_id int not null,
+make varchar (20) not null, 
+model varchar (20), 
+registration varchar (20) not null,
+colour varchar (20) not null, 
+primary key (car_id),
+unique (registration)
+); 
+explain cars_details;
